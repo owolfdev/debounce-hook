e4ea11c3b2cf13f1d7d45ef32fb414293c304bdb
owolfdev <owolfdev@gmail.com>
2024-05-18 07:11:01 +0700

    add link

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ const DebounceDemo = () => {
         type="text"
         value={inputValue}
         onChange={handleChange}
-        placeholder="Type something..."
+        placeholder="Type something here..."
         className="p-2 border border-gray-300 rounded"
       />
       <div>
@@ -26,17 +26,37 @@ const DebounceDemo = () => {
         <p className="text-sm pb-2">Half a second delay</p>
 
         <p className="text-2xl">
-          <span className="font-semibold">{debouncedValue}</span>
+          {debouncedValue ? (
+            <span className="font-semibold">{debouncedValue}</span>
+          ) : (
+            <span className="text-gray-400">Waiting for input...</span>
+          )}
         </p>
       </div>
       <div>
         <h4 className="font-bold text-xl">Output Without Debounce</h4>
         <p className="text-sm pb-2">No delay</p>
         <p className="text-2xl">
-          <span className="font-semibold">{inputValue}</span>
+          {inputValue ? (
+            <span className="font-semibold">{inputValue}</span>
+          ) : (
+            <span className="text-gray-400">Waiting for input...</span>
+          )}
         </p>
       </div>
-      <p>
+      <hr />
+      <div className="pt-8">
+        <p className="font-bold">Article about this debounce hook.</p>
+        <Link
+          title="Read Article"
+          target="_blank"
+          href="https://www.owolf.com/blog/react-debounce-hook"
+        >
+          https://www.owolf.com/blog/react-debounce-hook
+        </Link>
+      </div>
+      <div className="pt-8">
+        <p className="font-bold">Link to source code.</p>
         <Link
           title="View Source Code"
           target="_blank"
@@ -44,7 +64,7 @@ const DebounceDemo = () => {
         >
           https://github.com/owolfdev/debounce-hook/blob/main/hooks/useDebounce.ts
         </Link>
-      </p>
+      </div>
     </div>
   );
 };
